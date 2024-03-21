c336741c672093fa996f24df7443a2e2ca56202f
beingPro007 <120173992+beingPro007@users.noreply.github.com>
2024-03-21 20:39:19 +0530

    #3 :- Updated page.js
    Updated page.js in which we added our final footer and contact us option

--- src/app/page.js
+++ src/app/page.js
@@ -24,17 +24,20 @@ export default function Home() {
             dolore officiis deserunt ex vel provident odit quae cupiditate ipsum
             sequi esse tenetur, laudantium iste. Est necessitatibus odio impedit
             iusto autem fuga ut, adipisci fugiat!
-          </p>          
+          </p>
         </div>
       </section>
       <section className="max-w-2xl mx-auto mt-4 text-center">
-        <SectionHeadrs
-         subHeader ={'Don\'t hesitate'}
-         mainHeader ={'Contact us'}
-         />
-         <a href="tel :+919664513886">+91 96645 13886</a>
-
+        <SectionHeadrs subHeader={"Don't hesitate"} mainHeader={"Contact us"} />
+        <div className="mt-8 mb-8 text-4xl text-gray-500 underline">
+          <a href="tel :+919664513886">
+            +91 96645 13886
+          </a>
+        </div>
       </section>
+      <footer className="p-8 font-semibold text-center border-t mt-18 text-slate-500">
+        &copy; 2024 All Rights Reserved To Our Team
+      </footer>
     </>
   );
 }
